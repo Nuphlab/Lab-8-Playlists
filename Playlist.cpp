a41665b6816b0655f124306b8bf203b905477543
Nuphlab <56048443+Nuphlab@users.noreply.github.com>
2019-11-06 15:24:14 -0700

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -5,16 +5,33 @@
 using namespace std;
 
 Playlist::Playlist(string playlistName) {
-   name = playlistName;
+	name = playlistName;
 }
 Playlist::Playlist(string playlistName, vector<Song*> playlistPtr) {
-   name = playlistName;
-   songPtr = playlistPtr;
+	name = playlistName;
+	songPtr = playlistPtr;
 }
 /*void Playlist: GetSong(vector<Song*> pointerToSong) {
-   songPtr = pointerToSong;
-   return songPtr;
+songPtr = pointerToSong;
+return songPtr;
 } */
 string Playlist::GetName() {
-   return name;
+	return name;
+}
+void Playlist::AddSongToList(Song* song){
+	songPtr.push_back(song);
+}
+int Playlist::NumberOfSongs() {
+	return songPtr.size();
+}
+void Playlist::GetSongLines() {
+	for (int i = 0; i < songPtr.size(); ++i) {
+		cout << songPtr.at(i)->GetFirstLine() << endl;
+	}
+}
+Song* Playlist::GetPtr(int index) {
+	return songPtr.at(index);
+}
+int Playlist::GetSize() {
+	return songPtr.size();
 }
