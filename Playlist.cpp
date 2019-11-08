7ab262db00acb851b5b9d509bc3498a5636728fc
Nuphlab <56048443+Nuphlab@users.noreply.github.com>
2019-11-08 15:10:52 -0700

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -5,38 +5,40 @@
 using namespace std;
 
 Playlist::Playlist(string playlistName) {
-   name = playlistName;
+	name = playlistName;
 }
 Playlist::Playlist(string playlistName, vector<Song*> playlistPtr) {
-   name = playlistName;
-   songPtr = playlistPtr;
+	name = playlistName;
+	songPtr = playlistPtr;
 }
 Playlist::~Playlist() {
 }
 string Playlist::GetName() {
-   return name;
+	return name;
 }
 void Playlist::AddSongToList(Song* song) {
-   songPtr.push_back(song);
+	songPtr.push_back(song);
+	
 }
 int Playlist::NumberOfSongs() {
-   return songPtr.size();
+	return songPtr.size();
 }
 void Playlist::GetSongLines() {
-   for (int i = 0; i < songPtr.size(); ++i) {
-      cout << songPtr.at(i)->GetFirstLine() << endl;
-   }
+	for (int i = 0; i < songPtr.size(); ++i) {
+		cout << songPtr.at(i)->GetFirstLine() << endl;
+	}
 }
 Song* Playlist::GetPtr(int index) {
-   return songPtr.at(index);
+	return songPtr.at(index);
 }
 int Playlist::GetSize() {
-   return songPtr.size();
+	return songPtr.size();
 }
-void Playlist::RemovePlaylistSong(int songIndexNumber, vector<Song*>& song) {
-   for (int i = 0; i < song.size()) {
-      if (i == songIndexNumber) {
-         delete song.at(songIndexNumber);
-      }
-   }
+void Playlist::RemovePlaylistSong(int index) {
+	songPtr.erase(songPtr.begin() + index);
+}
+void Playlist::ListSingleSong() {
+	for (int i = 0; i < songPtr.size(); ++i) {
+		cout << i << ": " << songPtr.at(i)->GetName() << endl;
+	}
 }
